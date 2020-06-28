cf592b2fa443784a764bec2c6d36af5f12093d3d
Arthur <arthrp@users.noreply.github.com>
2020-06-28 18:15:32 +0300

    First code commit

---
+++ NetworkLatencyFixer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace NetworkLatencyFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                PrintUsage();
+                return;
+            }
+
+            var manager = new RegistryManager();
+            switch (args[0])
+            {
+                case "-l":
+                    var networks = manager.GetAllNetworks();
+                    foreach (var kv in networks)
+                    {
+                        Console.WriteLine($"{kv.Key} - {kv.Value}");
+                    }
+                    break;
+                case "-f":
+                    AddValuesToRegistry(manager, args[1]);
+                    break;
+                case "-d":
+                    RemoveValuesFromRegistry(manager, args[1]);
+                    break;
+                default:
+                    PrintUsage();
+                    break;
+            }
+        }
+
+        static void AddValuesToRegistry(RegistryManager manager, string interfaceId)
+        {
+            manager.SetValues(interfaceId);
+        }
+
+        static void RemoveValuesFromRegistry(RegistryManager manager, string interfaceId)
+        {
+            manager.DeleteValues(interfaceId);
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("NetworkLatencyFixer -l - List all network interfaces with ids");
+            Console.WriteLine("NetworkLatencyFixer -f <id> - Apply fix to interface with specified id");
+            Console.WriteLine("NetworkLatencyFixer -d <id> - Remove fix from interface with speciefied id");
+        }
+    }
+}
---
+++ NetworkLatencyFixer/RegistryManager.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkLatencyFixer
+{
+    class RegistryManager
+    {
+        public void SetValues(string interfaceId)
+        {
+            var keyId = @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\" + interfaceId;
+
+            SetValue(keyId, "TcpAckFrequency", 1, RegistryValueKind.DWord);
+        }
+
+        public Dictionary<string,string> GetAllNetworks()
+        {
+            var res = new Dictionary<string, string>();
+            const string basePath = @"SYSTEM\CurrentControlSet\Control\Network\{4D36E972-E325-11CE-BFC1-08002BE10318}";
+            using (var parent = Registry.LocalMachine.OpenSubKey(basePath))
+            {
+
+                foreach (var keyName in parent.GetSubKeyNames())
+                {
+                    var connRegKey = Registry.LocalMachine.OpenSubKey($"{basePath}\\{keyName}\\Connection");
+                    if (connRegKey == null)
+                        continue;
+
+                    var val = connRegKey.GetValue("Name");
+                    res.Add(keyName, val.ToString());
+                }
+
+                return res;
+            }
+        }
+
+        public void DeleteValues(string interfaceId)
+        {
+            var keyId = @"SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\" + interfaceId;
+            using (var key = Registry.LocalMachine.OpenSubKey(keyId, true))
+            {
+                key.DeleteValue("TcpAckFrequency");
+            }
+        }
+
+        private void SetValue(string keyId, string valueName, int valueValue, RegistryValueKind kind)
+        {
+            Registry.SetValue(keyId, valueName, valueValue, kind);
+            Console.WriteLine($"Setting {valueName} to {valueValue}");
+        }
+    }
+}
